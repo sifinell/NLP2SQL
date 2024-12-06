dd69ee8d49b913943f40f41935206c1845b29385
sifine <147918213+sifine@users.noreply.github.com>
2024-12-06 15:59:02 +0100

    retriever

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import streamlit as st
 from pathlib import Path
 from langchain_openai import AzureChatOpenAI
+from langchain_openai import AzureOpenAIEmbeddings
 from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain.agents.agent_toolkits import create_retriever_tool
 from langgraph.prebuilt import create_react_agent
+from langchain_core.vectorstores import InMemoryVectorStore
 from langchain import hub
 from dotenv import load_dotenv
 import os
 import json
+import ast
+import re
 
 # Load environment variables from a .env file
 load_dotenv()
@@ -16,6 +21,7 @@ load_dotenv()
 aoai_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
 aoai_key=os.getenv("AZURE_OPENAI_API_KEY")
 aoai_deployment=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME")
+aoai_embedding=os.getenv("AZURE_OPENAI_EMBEDDING_NAME")
 aoai_api_version=os.getenv("AZURE_OPENAI_API_VERSION")
 
 # Set Streamlit page configuration
@@ -38,17 +44,56 @@ def configure_db():
     db_filepath = (Path(__file__).parent / "Chinook.db").absolute()
     return SQLDatabase.from_uri(f"sqlite:///{db_filepath}")
 
+# Function to configure the vector stored used by the retriever tool
+def configure_vector_store(db):
+    def query_as_list(db, query):
+        res = db.run(query)
+        res = [el for sub in ast.literal_eval(res) for el in sub if el]
+        res = [re.sub(r"\b\d+\b", "", string).strip() for string in res]
+        return list(set(res))
+    
+    artists = query_as_list(db, "SELECT Name FROM Artist")
+    albums = query_as_list(db, "SELECT Title FROM Album")
+    embeddings = AzureOpenAIEmbeddings(
+        azure_endpoint=aoai_endpoint,
+        api_key=aoai_key
+    )
+    vector_store = InMemoryVectorStore(embeddings)
+    vector_store.add_texts(artists + albums)
+
+    return vector_store
+
 # Function to configure the tools for the agent
-def configure_tools(db, llm):
+def configure_tools(db, llm, vector_store):
     toolkit = SQLDatabaseToolkit(db=db, llm=llm)
+
+    retriever = vector_store.as_retriever(search_kwargs={"k": 5})
+    description = (
+        "Use to look up values to filter on. Input is an approximate spelling "
+        "of the proper noun, output is valid proper nouns. Use the noun most "
+        "similar to the search."
+    )
+    retriever_tool = create_retriever_tool(
+        retriever,
+        name="search_proper_nouns",
+        description=description,
+    )
+
     tools = toolkit.get_tools()
+    tools.append(retriever_tool)
+
     return tools
 
 # Function to configure the agent
 def configure_agent(llm, tools):
     prompt_template = hub.pull("langchain-ai/sql-agent-system-prompt")
     system_message = prompt_template.format(dialect="SQLite", top_k=5)
-    agent_executor = create_react_agent(llm, tools, state_modifier=system_message)
+    suffix = (
+    "If you need to filter on a proper noun like a Name, you must ALWAYS first look up "
+    "the filter value using the 'search_proper_nouns' tool! Do not try to "
+    "guess at the proper name - use this function to find similar ones."
+    )
+    agent_executor = create_react_agent(llm, tools, state_modifier=f"{system_message}\n\n{suffix}")
     return agent_executor
 
 # Function to format the message based on its type
@@ -81,7 +126,8 @@ def main():
     # Configure the database, language model, tools, and agent
     db = configure_db()
     llm = configure_llm()
-    tools = configure_tools(db, llm)
+    vector_store = configure_vector_store(db)
+    tools = configure_tools(db, llm, vector_store)
     agent = configure_agent(llm, tools)
 
     # Initialize message history or clear it if the button is pressed
